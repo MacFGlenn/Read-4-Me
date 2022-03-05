45d299673f33e1ec3c956871c88d0f20b2f3c742
MFG <macfglenn@gmail.com>
2022-03-05 18:53:48 -0500

    inquirer prompts set up

--- index.js
+++ index.js
@@ -3,13 +3,84 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  // Title
+  (title = "What is the name of your project?"),
+  // Description
+  (description = "Provide a description of the project"),
+  // Table of Contents no question needed just add auto
+  // Installation
+  (install = "Enter instructions for installation"),
+  // Usage
+  (usage = "Provide reasons to use the project"),
+  // License
+  (license = "Choose which license you would like to use"),
+  // Contributing
+  (contribute = "Insert guidelines for how to help contribute to this project"),
+  // Tests
+  (tests = "Add any tests to use with your project"),
+  // Questions
+  (username = "Enter your github username"),
+  (email = "Enter your email address"),
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: title,
+      validate: (titleInput) => {
+        if (titleInput) {
+          return true;
+        } else {
+          console.log("Please enter a title!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "description",
+      message: description,
+    },
+    {
+      type: "input",
+      name: "install",
+      message: install,
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: usage,
+    },
+    //licsense junk
+    {
+      type: "input",
+      name: "contributing",
+      message: contribute,
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: tests,
+    },
+    {
+      type: "input",
+      name: "username",
+      message: username,
+    },
+    {
+      type: "input",
+      name: "email",
+      message: email,
+    },
+  ]);
+}
 
 // Function call to initialize app
 init();
